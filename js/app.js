cfbd2240939ed1822e33cb79ae3c9c333c97d018
Shiyu Wang <shiyu3169@gmail.com>
2018-07-11 10:56:39 -0400

    dynamically render table

--- js/app.js
+++ js/app.js
@@ -29,9 +29,82 @@
 
 /* STUDENT APPLICATION */
 $(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+
+    var attendance = JSON.parse(localStorage.attendance);
+        
+
+    var days = attendance[Object.keys(attendance)[0]].length;
+    var students = Object.keys(attendance);
+
+    renderDays();
+    renderStudents();
+
+    // renderday row based on days variable
+    function renderDays() {
+        // render days
+        for(let i=1; i<=days;i++) {
+            let dayTh = document.createElement("th");
+            dayTh.innerText = i;
+            $('.missed-col').before(dayTh);
+        }
+    }
+
+    function renderStudents() {
+        for (student of students) {
+            // create student tr
+            let studentTr = document.createElement("tr");
+            studentTr.classList.add('student');
+            
+            // create student names
+            let nameTd = document.createElement("td");
+            nameTd.classList.add('name-col');
+            nameTd.innerText = student;
+
+            //append student name into student tr
+            studentTr.append(nameTd);
+
+            // Create checkboxes
+            for (let i=1;i<=days;i++) {
+                let checkboxTd = document.createElement("td");
+                checkboxTd.classList.add("attend-col");
+                checkboxTd.innerHTML = "<input type='checkbox'>"
+                studentTr.append(checkboxTd);
+            }
+            
+
+            //Create missed-col
+            let missedCol = document.createElement("td");
+            missedCol.classList.add("missed-col");
+            missedCol.innerText="0"; 
+
+            studentTr.append(missedCol);
+
+            // append student tr into tbody
+            $('tbody').append(studentTr);
+        }
+    }
+
+    let $allMissed = $('tbody .missed-col'),
+    $allCheckboxes = $('tbody input');
+    
+
+
+    // Count a student's missed days
+    function countMissing() {
+        $allMissed.each(function() {
+            var studentRow = $(this).parent('tr'),
+                dayChecks = $(studentRow).children('td').children('input'),
+                numMissed = 0;
+
+            dayChecks.each(function() {
+                if (!$(this).prop('checked')) {
+                    numMissed++;
+                }
+            });
+
+            $(this).text(numMissed);
+        });
+    }
 
     // Check boxes, based on attendace records
     $.each(attendance, function(name, days) {
@@ -65,21 +138,3 @@ $(function() {
 
     countMissing();
 }());
-
-
-// Count a student's missed days
-function countMissing() {
-    $allMissed.each(function() {
-        var studentRow = $(this).parent('tr'),
-            dayChecks = $(studentRow).children('td').children('input'),
-            numMissed = 0;
-
-        dayChecks.each(function() {
-            if (!$(this).prop('checked')) {
-                numMissed++;
-            }
-        });
-
-        $(this).text(numMissed);
-    });
-}
